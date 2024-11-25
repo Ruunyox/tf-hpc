25e329959d882223afadc46446ad3ca6a352e54a
ruunyox <None>
2024-11-25 17:17:53 +0100

    strategy scope re-wrap

--- tf_hpc/scripts/__main__.py
+++ tf_hpc/scripts/__main__.py
@@ -9,13 +9,40 @@ import absl.logging
 
 absl.logging.set_verbosity(absl.logging.ERROR)
 
-strategies = {"mirrored_strategy": tf.distribute.MirroredStrategy}
+strategies = {"mirrored_strategy": tf.distribute.MirroredStrategy, None: None}
 cross_device_ops = {
     "hierarchical_copy_all_reduce": tf.distribute.HierarchicalCopyAllReduce,
     "nccl_all_reduce": tf.distribute.NcclAllReduce,
     "reduction_to_one_device": tf.distribute.ReductionToOneDevice,
+    None: None,
 }
 cast_types = {"float32": tf.float32}
+
+
+def prepare_dataset(cfg: Dict) -> Tuple:
+    if not cfg.cache_data:
+        rc = tfds.ReadConfig(try_autocache=False)
+        cfg.dataset["read_config"] = rc
+    dataset = tfds.load(**cfg.dataset)
+    if isinstance(dataset, list):
+        if len(dataset) not in [1, 2]:
+            raise RuntimeError("Dataset splitting only supports two splits")
+        else:
+            if len(dataset) == 1:
+                train_dataset = dataset[0]
+                val_dataset = None
+            if len(dataset) == 2:
+                train_dataset = dataset[0]
+                val_dataset = dataset[1]
+    elif isinstance(dataset, dict):
+        train_dataset = dataset["train"]
+        val_dataset = None
+
+    if cfg.cache_data:
+        train_dataset = train_dataset.cache()
+        if val_dataset is not None:
+            val_dataset = val_dataset.cache()
+    return train_dataset, val_dataset
 
 
 def main():
@@ -49,44 +76,35 @@ def main():
             cross_device_ops=cross_device_ops[cfg.strategy.opts.cross_device_ops.op](
                 **cross_device_ops_opts
             )
+            if cfg.strategy.opts.cross_device_ops.op is not None
+            else None
         )
         with strategy.scope():
             cfg = parser.instantiate_classes(cfg)
             compile_opts = CompileBuilder(**cfg.compile)
             fit_opts = FitBuilder(**cfg.fit)
+
+            train_dataset, val_dataset = prepare_dataset(cfg)
             cfg.model_wrapper.model.compile(**compile_opts)
+            tf.config.list_physical_devices()
+            print(
+                f"Using strategy {strategy} with devices {strategy._extended._devices}"
+            )
+            cfg.model_wrapper.model.fit(
+                x=train_dataset, validation_data=val_dataset, **fit_opts
+            )
+
     else:
         cfg = parser.instantiate_classes(cfg)
         compile_opts = CompileBuilder(**cfg.compile)
         fit_opts = FitBuilder(**cfg.fit)
+
+        train_dataset, val_dataset = prepare_dataset(cfg)
         cfg.model_wrapper.model.compile(**compile_opts)
 
-    if not cfg.cache_data:
-        rc = tfds.ReadConfig(try_autocache=False)
-        cfg.dataset["read_config"] = rc
-    dataset = tfds.load(**cfg.dataset)
-    if isinstance(dataset, list):
-        if len(dataset) not in [1, 2]:
-            raise RuntimeError("Dataset splitting only supports two splits")
-        else:
-            if len(dataset) == 1:
-                train_dataset = dataset[0]
-                val_dataset = None
-            if len(dataset) == 2:
-                train_dataset = dataset[0]
-                val_dataset = dataset[1]
-    elif isinstance(dataset, dict):
-        train_dataset = dataset["train"]
-        val_dataset = None
-
-    if cfg.cache_data:
-        train_dataset = train_dataset.cache()
-        if val_dataset is not None:
-            val_dataset = train_dataset.cache()
-
-    cfg.model_wrapper.model.fit(
-        x=train_dataset, validation_data=val_dataset, **fit_opts
-    )
+        cfg.model_wrapper.model.fit(
+            x=train_dataset, validation_data=val_dataset, **fit_opts
+        )
 
 
 if __name__ == "__main__":
